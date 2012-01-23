19dfe8b2c4cad25a90ce51c6125a93501100edb3
frolovskij@gmail.com <frolovskij@gmail.com@ba0e269a-8e89-31f8-dfe2-120d6df3c3a1>
2012-01-23 19:57:58 +0000

    Password edit activity added (not tested yet)

--- NotesAndFolders/gen/com/notesandfolders/R.java
+++ NotesAndFolders/gen/com/notesandfolders/R.java
@@ -20,18 +20,25 @@ public final class R {
         public static final int explorer_file_icon=0x7f070001;
         public static final int explorer_file_name=0x7f070002;
         public static final int explorer_listview=0x7f070000;
-        public static final int explorer_options_add=0x7f070008;
-        public static final int explorer_options_close=0x7f07000a;
-        public static final int explorer_options_settings=0x7f070009;
+        public static final int explorer_options_add=0x7f07000e;
+        public static final int explorer_options_close=0x7f070010;
+        public static final int explorer_options_settings=0x7f07000f;
         public static final int header=0x7f070003;
         public static final int linearLayout1=0x7f070005;
         public static final int login_password=0x7f070006;
+        public static final int password_edit_confirmation=0x7f07000c;
+        public static final int password_edit_current=0x7f070009;
+        public static final int password_edit_new=0x7f07000a;
+        public static final int password_edit_save=0x7f07000d;
+        public static final int textView1=0x7f070008;
         public static final int textView2=0x7f070004;
+        public static final int textView3=0x7f07000b;
     }
     public static final class layout {
         public static final int explorer=0x7f030000;
         public static final int explorer_item=0x7f030001;
         public static final int login=0x7f030002;
+        public static final int password_edit=0x7f030003;
     }
     public static final class menu {
         public static final int explorer_options=0x7f060000;
@@ -39,6 +46,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int login_button=0x7f050004;
+        public static final int msg_password_confirmation_do_not_match=0x7f050006;
+        public static final int msg_wrong_password=0x7f050005;
         public static final int password_hint=0x7f050003;
         public static final int password_incorrect=0x7f050002;
         public static final int password_prompt=0x7f050001;
---
+++ NotesAndFolders/src/com/notesandfolders/PasswordActivity.java
@@ -0,0 +1,70 @@
+/*
+Copyright 2012 Фроловский Алексей
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This file is a part of Notes & Folders project.
+ */
+
+package com.notesandfolders;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PasswordActivity extends BaseActivity implements OnClickListener {
+	private Button saveButton;
+	private EditText currentPasswordEdit;
+	private EditText newPasswordEdit;
+	private EditText newPasswordConfirmationEdit;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.password_edit);
+
+		currentPasswordEdit = (EditText) findViewById(R.id.password_edit_current);
+		newPasswordEdit = (EditText) findViewById(R.id.password_edit_new);
+		newPasswordConfirmationEdit = (EditText) findViewById(R.id.password_edit_confirmation);
+
+		saveButton = (Button) findViewById(R.id.password_edit_save);
+		saveButton.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		if (v == saveButton) {
+			Settings s = new Settings(this);
+
+			String realCurrentPasswordHash = s.getPasswordSha1Hash();
+			String currentPasswordHash = Login.getSha1Digest(currentPasswordEdit.getText()
+					.toString());
+			String newPassword = newPasswordEdit.getText().toString();
+			String newPasswordConfirmation = newPasswordConfirmationEdit.getText().toString();
+
+			if (!realCurrentPasswordHash.equals(currentPasswordHash)) {
+				showAlert(R.string.msg_wrong_password);
+			} else {
+				if (!newPassword.equals(newPasswordConfirmation)) {
+					showAlert(R.string.msg_password_confirmation_do_not_match);
+				} else {
+					s.setPassword(newPassword);
+					finish();
+				}
+			}
+
+		}
+	}
+}
