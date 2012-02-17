827ab3ee41f78823bc7f567be44fe51a59d0d4bd
frolovskij@gmail.com <frolovskij@gmail.com@ba0e269a-8e89-31f8-dfe2-120d6df3c3a1>
2012-02-17 20:55:53 +0000

    Issue 15 fixed

--- trunk/NotesAndFolders/src/com/notesandfolders/ImportTask.java
+++ trunk/NotesAndFolders/src/com/notesandfolders/ImportTask.java
@@ -1,46 +1,82 @@
 package com.notesandfolders;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import android.os.AsyncTask;
 
 import com.notesandfolders.activities.ExplorerActivity;
+import com.notesandfolders.activities.ImportActivity;
 import com.notesandfolders.dataaccess.NodeHelper;
 
-public class ImportTask extends AsyncTask<Void, String, Integer> {
+public class ImportTask extends AsyncTask<Void, Integer, Integer> {
 
-	private ExplorerActivity mExplorer;
-	private NodeHelper mNh;
-	private long mIdToCopy;
-	private long mNewParentId;
+	private ImportActivity mExplorer;
 	private boolean completed;
 	private Integer result;
+	private File mFile;
 
-	public ImportTask(ExplorerActivity explorer, NodeHelper nh, long idToCopy,
-			long newParentId) {
-		mNh = nh;
-		mIdToCopy = idToCopy;
-		mNewParentId = newParentId;
+	public ImportTask(ImportActivity explorer, File file) {
 		mExplorer = explorer;
+		mFile = file;
+	}
+
+	protected void onProgressUpdate(Integer... progress) {
+		if (progress.length == 1) {
+			mExplorer.getImportingDialog().setProgress(progress[0]);
+		}
+
+		if (progress.length == 2) {
+			mExplorer.getImportingDialog().setProgress(progress[0]);
+			mExplorer.getImportingDialog().setMax(progress[1]);
+		}
 	}
 
 	@Override
 	protected Integer doInBackground(Void... arg0) {
-		result = mNh.copy(mIdToCopy, mNewParentId);
-		return result;
+		final NodeHelper nh = new NodeHelper(mExplorer,
+				Login.getPlainTextPasswordFromTempStorage(mExplorer));
+
+		Node importRoot = nh.createFolder(nh.getRootFolder(), "Imported at "
+				+ new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").format(new Date()));
+
+		final List<Node> nodes = FileImporter.getFiles(mFile.getAbsolutePath(), nh.getLastId() + 1,
+				importRoot.getId());
+
+		// if there's nothing to import
+		if (nodes.size() == 0) {
+			nh.deleteNodeById(importRoot.getId());
+		} else {
+
+			int nodesCount = nodes.size();
+			publishProgress(0, nodesCount);
+
+			for (int i = 0; i < nodesCount; i++) {
+				Node n = nodes.get(i);
+
+				nh.insertNode(n);
+				publishProgress(i + 1, nodesCount);
+			}
+		}
+
+		return 0;
 	}
 
 	@Override
 	protected void onPreExecute() {
-		mExplorer.showDialog(ExplorerActivity.COPYING_DIALOG_ID);
+		mExplorer.showDialog(ImportActivity.IMPORTING_DIALOG_ID);
 	}
 
 	@Override
 	protected void onPostExecute(Integer res) {
-		mExplorer.dismissDialog(ExplorerActivity.COPYING_DIALOG_ID);
+		mExplorer.dismissDialog(ImportActivity.IMPORTING_DIALOG_ID);
 
 		notifyActivityTaskCompleted();
 	}
 
-	public void setActivity(ExplorerActivity explorer) {
+	public void setActivity(ImportActivity explorer) {
 		this.mExplorer = explorer;
 		if (completed) {
 			notifyActivityTaskCompleted();
