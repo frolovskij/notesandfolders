dfd7cee733a1571f5ae6f793f0fc0dffcc73809b
frolovskij@gmail.com <frolovskij@gmail.com@ba0e269a-8e89-31f8-dfe2-120d6df3c3a1>
2012-02-20 14:39:26 +0000

    equals() & hashCode() for CheckList & CheckListItem

--- NotesAndFolders/src/com/notesandfolders/CheckListItem.java
+++ NotesAndFolders/src/com/notesandfolders/CheckListItem.java
@@ -47,4 +47,26 @@ public class CheckListItem implements Serializable {
 		this.text = text;
 		this.isChecked = isChecked;
 	}
+
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null) {
+			return false;
+		}
+		if (!(o instanceof CheckListItem)) {
+			return false;
+		}
+		CheckListItem otherObj = (CheckListItem) o;
+		return (isChecked() == otherObj.isChecked() && getText().equals(
+				otherObj.getText()));
+	}
+
+	public int hashCode() {
+		int result = 17;
+		result = 31 * result + getText().hashCode();
+		result = 31 * result + (isChecked() ? 1 : 0);
+		return result;
+	}
 }
