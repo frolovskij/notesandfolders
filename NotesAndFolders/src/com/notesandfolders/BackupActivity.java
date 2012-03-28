dbbbd041a7837c62c52c379df3dd1724fa0b329a
frolovskij@gmail.com <frolovskij@gmail.com@ba0e269a-8e89-31f8-dfe2-120d6df3c3a1>
2012-03-28 12:52:33 +0000

    BackupTask

--- NotesAndFolders/src/com/notesandfolders/BackupActivity.java
+++ NotesAndFolders/src/com/notesandfolders/BackupActivity.java
@@ -19,12 +19,25 @@ This file is a part of Notes & Folders project.
 package com.notesandfolders;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
 public class BackupActivity extends Activity {
+	public static final int DIALOG_BACKUP = 1;
 	private TextView info;
+	private Button backupButton;
+	private boolean mShownDialog;
+	private ProgressDialog backupDialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -34,5 +47,49 @@ public class BackupActivity extends Activity {
 
 		info = (TextView) findViewById(R.id.backup_info);
 		info.setText(Html.fromHtml(getText(R.string.backup_info).toString()));
+
+		backupButton = (Button) findViewById(R.id.backup_button);
+		backupButton.setOnClickListener(onBackupButtonListener);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_BACKUP) {
+			backupDialog = new ProgressDialog(this);
+			backupDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			backupDialog.setMessage(getText(R.string.backup_msg_in_progress));
+			backupDialog.setCancelable(false);
+
+			NodeHelper nh = new NodeHelper(this,
+					new TempStorage(this).getPassword());
+			backupDialog.setMax((int) nh.getNodesCount());
+
+			return backupDialog;
+		}
+
+		return super.onCreateDialog(id);
+	}
+
+	public void onBackupTaskCompleted(Integer result) {
+		if (mShownDialog) {
+			checkBackupResult(result);
+		}
+	}
+
+	public void checkBackupResult(int result) {
+		System.out.println(result);
+	}
+
+	private final OnClickListener onBackupButtonListener = new OnClickListener() {
+		public void onClick(View v) {
+			BackupTask task = new BackupTask(BackupActivity.this,
+					new NodeHelper(BackupActivity.this, new TempStorage(
+							BackupActivity.this).getPassword()));
+			task.execute();
+		}
+	};
+	
+	public ProgressDialog getBackupDialog() {
+		return backupDialog;
 	}
 }
